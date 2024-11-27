a16951351585975524b36698b17cfdd8bccda4e7
alexofs <53371198+alexofs@users.noreply.github.com>
2024-11-27 09:47:24 -0600

    Add files via upload

--- demo-qry.sql
+++ demo-qry.sql
@@ -15,3 +15,19 @@ from   S_EMP;
 -- DEPT_LOV
 select NAME, ID
 from   S_DEPT;
+
+-- Pie chart:
+select  r.NAME, count(*)
+from    S_REGION r, S_CUSTOMER c
+where   r.ID = c.REGION_ID
+group   by r.NAME;
+
+-- Bar chart:
+select  c.NAME, count(*)
+from    S_CUSTOMER c, S_ORD o
+where   c.ID = o.CUSTOMER_ID
+group   by c.NAME
+order   by 2 desc;
+
+-- Adding BLOB column
+alter table S_PRODUCT add (IMAGE BLOB);
